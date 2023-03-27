3ac762ee385b6125da05d5bc3a95dae6555991fc
Narek Pogosian <narekpogosian2@gmail.com>
2023-03-27 11:22:27 +0200

    er

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Comment = require("../../models/Comment");
+const Errand = require("../../models/Errand");
 
 router.post("/", async (req, res) => {
   const { caseId, email, body } = req.body;
--- schemas/errandsSchema.js
+++ schemas/errandsSchema.js
@@ -1,10 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const errandSchema = mongoose.Schema({
-    email: { type: String, required: true},
-    subject: { type: String, required: true},
-    message: { type: String, required: true},
-    status: { type: Number, default: 1}
-}, { timestamp: true })
+const errandSchema = mongoose.Schema(
+  {
+    email: { type: String, required: true },
+    subject: { type: String, required: true },
+    message: { type: String, required: true },
+    status: { type: Number, default: 1 },
+  },
+  { timestamp: true }
+);
 
-module.exports = mongoose.model('Errand', errandSchema)
+const Errand = mongoose.model("Errand", errandSchema);
+
+module.exports = Errand;
